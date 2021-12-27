d559cb8ce7628c217129206bab2b6e7c41dd3264
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-26 23:20:27 -0600

    initial commit

---
+++ README.js
@@ -0,0 +1,10 @@
+# Code War Algorithm Narcissistic Number Read Me
+
+```
+A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base.
+```
+
+## The challenge:
+```
+Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
+```
