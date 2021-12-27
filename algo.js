7f868e24eadeaa9cac5ac7340f895080b7804c0d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-26 23:31:44 -0600

    comment on function

---
+++ algo.js
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////
+//  Code Wars: Narcissistic Number
+//  By: Virgilio D> Cabading Jr.
+////////////////////////////////////////////////////////
+
+// Code me to return true or false
+/**
+ * Narcissistic Function
+ * @param {int} value 
+ * Find if value is a narcissistic number
+ */
+
+function narcissistic(value) {
+    let 
+  }
+  
